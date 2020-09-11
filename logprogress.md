d2fa555c1cd1af82dbb57afb2b94d16cea66d2b1
Vincent-Projects <crysyroodpro@outlook.com>
2020-09-11 16:21:24 +0200

    React Burger Builder app initiated

--- logprogress.md
+++ logprogress.md
@@ -1,3 +1,5 @@
 # 2020, September 11
 Start of the challenge.
 I want to learn and improve my skills with React, Nodejs, Express, SQL and NoSQL, REST API. I want to build complete projects.
+
+I started the burger builder react app from Maximilian Schwarzmüller's react course. I wanted to build my own burger and realized that i'm struggling with making it responsive. I think that i will go deeper with css and media queries.
---
+++ projects/burger-builder/src/components/Burger/Burger.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.module.css';
+
+const Burger = () => {
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="salad" />
+            <BurgerIngredient type="pickle" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="bread-bottom" />
+        </div >
+    );
+}
+
+export default Burger;
